b7f11f1d2c6a3768d08cc70f6a81dd915875dfbd
MAIHANJAN ABDUL QAYOOM <14018276@studentmail.ul.ie>
2019-02-27 02:37:50 +0000

    dragablle cards are add for candidate selection

---
+++ Fingerprint_Voting/Fingerprint_Voting/Controllers/VoteController.cs
@@ -0,0 +1,32 @@
+﻿using Fingerprint_Voting.Models;
+using Fingerprint_Voting.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fingerprint_Voting.Controllers
+{
+    public class VoteController : Controller
+    {
+      
+        // GET: Vote
+        public ActionResult Index()
+        {
+            
+            CandidatesViewModel candidateVM = new CandidatesViewModel();
+            List<CandidateDTO> candidates = candidateVM.GetAllCandidates();
+
+            String userId = User.Identity.GetUserId();
+
+
+
+            ViewBag.userId = userId; 
+
+            return View(candidates);
+        }
+    }
+}
---
+++ Fingerprint_Voting/Fingerprint_Voting/MyScripts/voteScripts.js
@@ -0,0 +1,100 @@
+﻿function dragMoveListener(event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    //console.log(x)
+}
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '#yes-drop',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
+
+    // listen for drop related events:
+
+    ondropactivate: function (event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active');
+    },
+    ondragenter: function (event) {
+        var draggableElement = event.relatedTarget,
+            dropzoneElement = event.target;
+
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target');
+        draggableElement.classList.add('can-drop');
+        draggableElement.textContent = 'Dragged in';
+
+
+
+    },
+    ondragleave: function (event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
+        event.relatedTarget.classList.remove('can-drop');
+        event.relatedTarget.textContent = 'Dragged out';
+        // reload the page when the user draged out the candidate card from the droped zone
+        location.reload();
+    },
+    ondrop: function (event) {
+        //console.log(event)
+
+        var result = event.relatedTarget.textContent = "Vote"; 
+      
+
+
+        if (result == 'Vote') {
+            
+            var confirmation = confirm("Would you like to vote for the selected candidate?");
+            if (confirmation) {
+                console.log("When user pressed ok than redirect the user to take his/her fingerprint to validate if he is the user giving vote ");
+                window.location.replace('https://flaviocopes.com/how-to-redirect-using-javascript/')
+            } else {
+                location.reload();
+            }
+            
+        } 
+        // Add event handler
+       
+            //$("yes-drop").prop('disabled', true);
+            //$("dropzone").prop("disabled", true);
+            //$("drag-drop").prop("disabled", true);
+            //$("dropzone").prop("disabled", true);
+
+            console.log("you are voting right now")
+        
+        
+    },
+    ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-target');
+    }
+});
+
+interact('.drag-drop')
+    .draggable({
+        inertia: true,
+        restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 0, right: 0 }
+        },
+        autoScroll: true,
+        // dragMoveListener from the dragging demo above
+        onmove: dragMoveListener,
+    });
+
